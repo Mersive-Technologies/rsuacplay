63d1edca0ade245a423dcc1261c99856dd51e5ba
Brent Gardner <bgardner@squarelabs.net>
2020-04-12 12:58:48 -0600

    It compiles... ship it!

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,25 @@
 extern crate libusb_sys as ffi;
 
-use ffi::{libusb_context, libusb_device_handle, LIBUSB_SUCCESS, libusb_transfer};
+use core::mem;
+use std::os::raw::{c_uchar, c_uint};
 use std::ptr::{null, null_mut};
 use std::sync::Mutex;
-use core::mem;
 
-const SPKR_EP: i32 = 1;
+use ffi::{libusb_context, libusb_device_handle, LIBUSB_SUCCESS, libusb_transfer, LIBUSB_TRANSFER_TYPE_ISOCHRONOUS};
+
+const SPKR_EP: u8 = 1;
 const ID_VENDOR: u16 = 0x046d;
 const ID_PRODUCT: u16 = 0x0867;
 const SAMPLE_RATE: u32 = 32000;
 const CTRL_IFACE: i32 = 0;
 const SPKR_IFACE: i32 = 2;
-const ISO_PKT_PER_FRAME: i32 = 6;
-const BYTES_PER_ISO_PKT: i32 = 128;
-const TIMEOUT: i32 = 1000;
+const ISO_PKT_PER_FRAME: u32 = 6;
+const BYTES_PER_ISO_PKT: u32 = 128;
+const TIMEOUT: c_uint = 1000;
 const A4: f32 = 440.0f32;
-const CHANNEL_CNT: i32 = 2;
+const CHANNEL_CNT: u32 = 2;
 const TAU: f32 = std::f32::consts::PI * 2.0f32;
-const BUFFER_SZ: usize = (BYTES_PER_ISO_PKT * BYTES_PER_ISO_PKT) as usize;
+const BUFFER_SZ: usize = (BYTES_PER_ISO_PKT * ISO_PKT_PER_FRAME) as usize;
 
 type SampleSize = i16;
 
@@ -25,7 +27,7 @@ type SampleSize = i16;
 // cons: some pointer ceremony, more docs and less S/O, made me think about allocation
 
 struct PlayState {
-    samples_played: Mutex<i32>,
+    samples_played: Mutex<u32>,
     buffers: [[SampleSize; BUFFER_SZ]; 3],
     xfers: [*mut libusb_transfer; 3],
 }
@@ -67,39 +69,45 @@ fn main() {
         // play
         let mut state = PlayState::new();
         for i in 0..3 {
-            state.xfers[i] = ffi::libusb_alloc_transfer(ISO_PKT_PER_FRAME);
-//            ffi::libusb_set_iso_packet_lengths( state.xfers[i], BYTES_PER_ISO_PKT );
-            fill_xfer(&state, i);
+            state.xfers[i] = ffi::libusb_alloc_transfer(ISO_PKT_PER_FRAME as i32);
+            fill_xfer(&state, i, dev_handle);
         }
     }
 }
 
-unsafe fn fill_xfer(play_state: &PlayState, buff_idx: usize) {
+unsafe fn fill_xfer(play_state: &PlayState, buff_idx: usize, dev_handle: *mut libusb_device_handle) {
     let mut samples_played = play_state.samples_played.lock().unwrap();
 
     let byte_cnt = BYTES_PER_ISO_PKT * ISO_PKT_PER_FRAME;
-    let sample_cnt = byte_cnt / CHANNEL_CNT / mem::size_of::<SampleSize>() as i32;
+    let sample_cnt = byte_cnt / CHANNEL_CNT / mem::size_of::<SampleSize>() as u32;
     let max_samp_vol = SampleSize::max_value();
     for i in 0..sample_cnt {
         let seconds = (*samples_played + i) as f32 / SAMPLE_RATE as f32;
         let cycles = A4 * seconds;
-        let samp = (cycles * TAU).sin() * max_samp_vol;
-        play_state.buffers[buff_idx][i] = samp;
+        let samp = (cycles * TAU).sin() * (max_samp_vol as f32);
+        let mut buff = play_state.buffers[buff_idx];
+        buff[i as usize] = samp as i16;
     }
     *samples_played += sample_cnt;
 
-    ffi::libusb_fill_iso_transfer(
-        play_state.xfers[buff_idx],
-        dev_handle,
-        SPKR_EP,
-        play_state.buffers[buff_idx],
-        byte_cnt,
-        ISO_PKT_PER_FRAME,
-        xferComplete,
-        this,
-        TIMEOUT,
-    );
+    let buff: *const [SampleSize; BUFFER_SZ] = &play_state.buffers[buff_idx];
+    let xfer = play_state.xfers[buff_idx];
+    (*xfer).dev_handle = dev_handle;
+    (*xfer).endpoint = SPKR_EP;
+    (*xfer).transfer_type = LIBUSB_TRANSFER_TYPE_ISOCHRONOUS;
+    (*xfer).timeout = TIMEOUT;
+    (*xfer).buffer = buff as *mut c_uchar;
+    (*xfer).length = byte_cnt as i32;
+    (*xfer).num_iso_packets = ISO_PKT_PER_FRAME as i32;
+    (*xfer).callback = xfer_complete;
+//    play_state.xfers[buff_idx].user_data = this;
+
+    for i in 0..(*xfer).num_iso_packets {
+        (*xfer).iso_packet_desc[i as usize].length = BYTES_PER_ISO_PKT;
+    }
 }
+
+extern "C" fn xfer_complete(transfer: *mut libusb_transfer) {}
 
 unsafe fn detach_kernel(dev_handle: *mut libusb_device_handle, iface_id: i32) {
     if ffi::libusb_kernel_driver_active(dev_handle, iface_id) != 1 {
